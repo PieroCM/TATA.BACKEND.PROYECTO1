00618b509025d23fbb75ea013a485130ebed1d5a
PieroCM <pierojesus_20@hotmail.com>
2025-11-10 14:22:07 -0500

    Creacion de Repository de Solicitud

---
+++ TATA.BACKEND.PROYECTO1.CORE/Infraestructure/Repository/RepositorySolicitud.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TATA.BACKEND.PROYECTO1.CORE.Infraestructure.Repository
+{
+    internal class RepositorySolicitud
+    {
+    }
+}
