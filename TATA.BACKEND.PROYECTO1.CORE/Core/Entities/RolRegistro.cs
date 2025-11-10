7ad24efc09c39f10f41fa1a94204d7f8f1ac1494
Lucero Ayala <22200171@ue.edu.pe>
2025-11-10 13:34:28 -0500

    Crud services y crud repository Usuario

---
+++ TATA.BACKEND.PROYECTO1.CORE/Core/Entities/RolRegistro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TATA.BACKEND.PROYECTO1.CORE.Core.Entities;
+
+public partial class RolRegistro
+{
+    public int IdRolRegistro { get; set; }
+
+    public string NombreRol { get; set; } = null!;
+
+    public string? BloqueTech { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public bool EsActivo { get; set; }
+
+    public virtual ICollection<Solicitud> Solicitud { get; set; } = new List<Solicitud>();
+}
---
+++ TATA.BACKEND.PROYECTO1.CORE/Core/Entities/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using TATA.BACKEND.PROYECTO1.CORE.Infraestructure.Data;
+
+namespace TATA.BACKEND.PROYECTO1.CORE.Core.Entities;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public int IdRolSistema { get; set; }
+
+    public string? Estado { get; set; }
+
+    public DateTime? UltimoLogin { get; set; }
+
+    public DateTime CreadoEn { get; set; }
+
+    public DateTime? ActualizadoEn { get; set; }
+
+    public virtual RolesSistema IdRolSistemaNavigation { get; set; } = null!;
+
+    public virtual ICollection<LogSistema> LogSistema { get; set; } = new List<LogSistema>();
+
+    public virtual ICollection<Personal> Personal { get; set; } = new List<Personal>();
+
+    public virtual ICollection<Reporte> Reporte { get; set; } = new List<Reporte>();
+
+    public virtual ICollection<Solicitud> Solicitud { get; set; } = new List<Solicitud>();
+}
